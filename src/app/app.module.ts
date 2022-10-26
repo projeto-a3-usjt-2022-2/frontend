9887409bfbadf40ae11083da9a1c53005f5a5e05
Léo Martins <leonardomartinsleo@yahoo.com.br>
2022-10-26 15:08:32 -0300

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 import { RegisterScreenComponent } from './components/register-screen/register-screen.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginScreenComponent, RegisterScreenComponent],
+  declarations: [AppComponent, LoginScreenComponent, RegisterScreenComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
