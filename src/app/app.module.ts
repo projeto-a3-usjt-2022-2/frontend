8501fed43068c13e099fef3b4829e10536293629
Léo Martins <leonardomartinsleo@yahoo.com.br>
2022-11-04 23:37:31 -0300

    feat: create login screen and function to verify if user exist on database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule  } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +8,12 @@ import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 import { RegisterScreenComponent } from './components/register-screen/register-screen.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, LoginScreenComponent, RegisterScreenComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LoginScreenComponent, RegisterScreenComponent, HomeComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/login-screen/login-screen.component.ts
+++ src/app/components/login-screen/login-screen.component.ts
@@ -1,15 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
-  styleUrls: ['./login-screen.component.scss']
+  styleUrls: ['./login-screen.component.scss'],
 })
 export class LoginScreenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  credentialError: null | string = null;
+  errorLogin = '';
+  dataLogin: null | any = null;
 
-  ngOnInit(): void {
+  async loginUser(loginForm: { login: string; password: string }) {
+    this.credentialError = 'Deu erro no cpf';
+    try {
+      let { data, status } = await axios.post('http://localhost:8080/login', {
+        credential: loginForm.login,
+        password: loginForm.password,
+      });
+
+      return (this.dataLogin = data);
+    } catch (error: any) {
+      this.errorLogin = error.response.data.message;
+      throw new Error();
+    }
   }
-
 }
