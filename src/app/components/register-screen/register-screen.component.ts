9c579ee721370e2dbf68f9fc976e30577aa0ea0d
Léo Martins <leonardomartinsleo@yahoo.com.br>
2022-11-07 08:46:22 -0300

    ng Form register screen

--- src/app/components/register-screen/register-screen.component.ts
+++ src/app/components/register-screen/register-screen.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-register-screen',
   templateUrl: './register-screen.component.html',
   styleUrls: ['./register-screen.component.scss']
 })
 export class RegisterScreenComponent implements OnInit {
-
+  
+  dataLogin: null | any = null;
+  errorLogin = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  async registerUser(registerForm:{
+    name: string;
+    lastname: string;
+    cpf:string;
+    birthDate: any;
+    sex: string;
+    convenio: string;
+    email: string;
+    remail: string;
+    password: string;
+    repassword: string }) {
+      try {
+        let { data, status } = await axios.post('http://localhost:8081/createUser', {
+          credential: registerForm.name,
+          password: registerForm.password,
+        });
+  
+        return (this.dataLogin = data);
+      } catch (error: any) {
+  
+        
+        this.errorLogin = error.response.data.message;
+        
+      }
+ }
 }
